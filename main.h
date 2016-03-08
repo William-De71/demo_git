0baf6ae1498923e1253af86d1d0cdfdc78f4fd5d
William-De71 <williamderen@gmail.com>
2016-03-08 13:35:27 +0100

    Ajouté fichier exemple main.h

---
+++ main.h
@@ -0,0 +1,19 @@
+/*
+TITRE : main.h
+AUTEUR : William DEREN
+PROJET : Jeu du Plus ou Moins
+DATE : 17/01/2015
+*/
+#ifndef __MAIN
+#define __MAIN
+
+
+int testvaleur();
+int getNbJoueurs();
+int getDifficulte();
+int mnuQuit();
+void Resultat(int joueur);
+int getNbMystere1(int joueur);
+
+
+#endif
